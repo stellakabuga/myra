79e5cf2234f19daa525a1132e63b47ad50433bed
stellakabuga <125837282+stellakabuga@users.noreply.github.com>
2023-02-19 18:54:06 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "storage" {
+    description = "update rds storage"
+    type = string
+    default = "80"
+  
+}
+
+variable "username" {
+    description = "update rds username"
+    type = string
+    default = "admin"
+  
+}
+
+variable "db_name" {
+    description = "update rds db_name"
+    type = string
+    default = "myra"
+  
+}
